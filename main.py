a87fa0016373ddd7379c2f47be7e65076386d7ce
Huykka <huypoilkj@gmail.com>
2023-05-31 09:20:02 +0700

    .

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import random
 
 LOOPS = 100 # số lần lặp
 n = 3 # số lần chạy bài toán
-nbOfCity = 20 # số lượng thành phố
-nbOfAnt = 10 # số lượng kiến
+nbOfCity = 10 # số lượng thành phố
+nbOfAnt = 20 # số lượng kiến
 P = 0.5 # hệ số bay hơi [0, 1]
 Q = 1 # hệ số cường độ pheromone
 bestDistance = 0 # Khoảng cách tôi ưu ban đầu
